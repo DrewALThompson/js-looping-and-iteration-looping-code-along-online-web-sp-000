011d559cbd32a23efeb3f449c2f2393495a773d8
Drew Thompson <drewallynthompson@gmail.com>
2020-12-31 09:25:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-function writeCards(array, event ){
+function writeCards(array, event){
   let arr = []
   for(let i = 0; i < array.length; i++){
     arr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
