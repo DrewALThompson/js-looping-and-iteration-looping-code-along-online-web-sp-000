3608ea43a72c6399c19a0f05d154978e9d9cb138
Drew Thompson <drewallynthompson@gmail.com>
2020-12-31 09:23:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ function writeCards(array, event ){
   let arr = []
   for(let i = 0; i < array.length; i++){
     arr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    console.log(arr)
   };
 };
